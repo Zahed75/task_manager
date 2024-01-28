dd76612bd3939df5907bedf97362f4f6e18b88ed
“Zahed75” <zahedhasan.cs@gmail.com>
2024-01-28 17:00:18 +0600

    modified:   lib/api/apiClients.dart 	new file:   lib/utility/utility.dart

---
+++ lib/api/apiClients.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+import 'package:task_manager/style/style.dart';
+import 'package:http/http.dart' as http;
+import '../utility/utility.dart';
+
+var BaseURL="https://task.teamrabbil.com/api/v1";
+var RequestHeader={"Content-Type":"application/json"};
+
+
+//Login API calling
+
+Future<bool> LoginRequest(FormValues) async{
+  var URL=Uri.parse("${BaseURL}/login");
+  var PostBody=json.encode(FormValues);
+  var response= await http.post(URL,headers:RequestHeader,body: PostBody);
+  var ResultCode=response.statusCode;
+  var ResultBody=json.decode(response.body);
+  if(ResultCode==200 && ResultBody['status']=="success"){
+    SuccessToast("Request Success");
+   return true;
+  }
+  else{
+    ErrorToast("Request fail ! try again");
+    return false;
+  }
+}
+
+
+
+// Registration API
+
+
+Future<bool> RegistrationRequest(FormValues) async{
+  var URL=Uri.parse("${BaseURL}/registration");
+  var PostBody=json.encode(FormValues);
+
+  var response= await http.post(URL,headers:RequestHeader,body: PostBody);
+
+  var ResultCode=response.statusCode;
+
+  var ResultBody=json.decode(response.body);
+
+  if(ResultCode==200 && ResultBody['status']=="success"){
+    SuccessToast("Request Success");
+   return true;
+  }
+  else{
+    ErrorToast("Request fail ! try again");
+    return false;
+  }
+}
+
+// Recover Verify Email Request
+
+Future<bool> VerifyEmailRequest(Email) async{
+  var URL=Uri.parse("${BaseURL}/RecoverVerifyEmail/${Email}");
+  var response= await http.get(URL,headers:RequestHeader);
+  var ResultCode=response.statusCode;
+  var ResultBody=json.decode(response.body);
+  if(ResultCode==200 && ResultBody['status']=="success"){
+  
+    SuccessToast("Request Success");
+    return true;
+  }
+  else{
+    ErrorToast("Request fail ! try again");
+    return false;
+  }
+}
+
+
+// Verify OTP Request
+
+Future<bool> VerifyOTPRequest(Email,OTP,async){
+
+}
