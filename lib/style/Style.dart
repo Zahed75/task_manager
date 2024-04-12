15a0990956ec12b31635c3cb6ae461801bdfed5d
“Zahed75” <zahedhasan.cs@gmail.com>
2024-04-12 20:21:07 +0600

    color added

---
+++ lib/style/Style.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+
+const colorRed=Color.fromRGBO(231, 28, 36, 1);
+const colorGreen=Color.fromRGBO(33, 191, 115, 1);
+const colorWhite=Color.fromRGBO(255, 255, 255, 1);
+const colorDarkBlue=Color.fromRGBO(44, 62, 80, 1);
+const colorBlue=Color.fromRGBO(52, 152, 219, 1.0);
+const colorLight=Color.fromRGBO(211,211, 211, 1.0);
+const colorLightGray=Color.fromRGBO(135, 142, 150, 1.0);
+
+
